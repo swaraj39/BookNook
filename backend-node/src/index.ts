9f8a2c186b0b69f018650ab20bf0680c16a60ad5
Shikhar <shikharm.3110@gmail.com>
2026-06-24 19:16:56 +0530

    URL updated to Deployement

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -11,17 +11,18 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-const frontendUrl = "http://localhost:5173";
+// const frontendUrl = "http://localhost:5173";
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      frontendUrl,
       "http://127.0.0.1:5173",
     ],
     credentials: true,
   })
 );
-app.use(cookieParser());
 
 app.use(cookieParser());
 app.use(express.json());
