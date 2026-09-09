132d12a848808f8c15b491cc25de3b0218da0fd4
swaraj39 <swarajgujar022@gmail.com>
2026-09-09 11:35:57 +0530

    sedf

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -95,7 +95,7 @@ app.use((req, res) => {
 });
 app.use(errorHandler);
 
-cron.schedule("0 11 * * *", async () => {
+cron.schedule("45 11 * * *", async () => {
   console.log("Running re-engagement reminder job...");
   try {
     await ReminderService.processReminders();
